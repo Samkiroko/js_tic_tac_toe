6c2ac2112d799dbd509afa9c7b18d32155a8e9c6
Samkiroko <kirokodev@gmail.com>
2020-07-17 00:16:48 +0300

    seperating DOM form the Logic

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-const X_CLASS = 'x';
-const CIRCLE_CLASS = 'circle';
-const WINNING_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-const cellElements = document.querySelectorAll('[data-cell]');
-const board = document.getElementById('board');
-const winningMessageElement = document.getElementById('winningMessage');
-const restartButton = document.getElementById('restartButton');
-const winningMessageTextElement = document.querySelector(
-  '[data-winning-message-text]',
-);
 let circleTurn;
 
 startGame();
@@ -59,9 +40,10 @@ function endGame(draw) {
 }
 
 function isDraw() {
-  return [...cellElements].every((cell) => (
-    cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
-  ));
+  return [...cellElements].every(
+    (cell) =>
+      cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  );
 }
 
 function placeMark(cell, currentClass) {
@@ -83,5 +65,9 @@ function setBoardHoverClass() {
 }
 
 function checkWin(currentClass) {
-  return WINNING_COMBINATIONS.some((combination) => combination.every((index) => cellElements[index].classList.contains(currentClass)));
+  return WINNING_COMBINATIONS.some((combination) =>
+    combination.every((index) =>
+      cellElements[index].classList.contains(currentClass)
+    )
+  );
 }
