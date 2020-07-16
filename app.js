23b81da4334ebcf8100eef2695cc29ebd543c79b
Samkiroko <kirokodev@gmail.com>
2020-07-16 15:28:01 +0300

    complite implementation of the javascript

--- app.js
+++ app.js
@@ -1,26 +1,69 @@
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle';
-const cellElements = document.querySelectorAll('[data-cell');
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
+const winningMessageElement = document.getElementById('winningMessage');
+const restartButton = document.getElementById('restartButton');
+const winningMessageTextElement = document.querySelector(
+  '[data-winning-message-text]'
+);
 let circleTurn;
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
 
 function startGame() {
   circleTurn = false;
   cellElements.forEach((cell) => {
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(CIRCLE_CLASS);
+    cell.removeEventListener('click', handleClick);
     cell.addEventListener('click', handleClick, { once: true });
   });
-  setBoardHover();
+  setBoardHoverClass();
+  winningMessageElement.classList.remove('show');
 }
 
 function handleClick(e) {
   const cell = e.target;
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
   placeMark(cell, currentClass);
-  // check for Win
-  // check for Draw
-  // switch Turns
-  swapTurns();
-  setBoardHoverClass();
+  if (checkWin(currentClass)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    swapTurns();
+    setBoardHoverClass();
+  }
+}
+
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = 'Draw!';
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
+  }
+  winningMessageElement.classList.add('show');
+}
+
+function isDraw() {
+  return [...cellElements].every((cell) => {
+    return (
+      cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    );
+  });
 }
 
 function placeMark(cell, currentClass) {
@@ -31,7 +74,7 @@ function swapTurns() {
   circleTurn = !circleTurn;
 }
 
-function setBoardHover() {
+function setBoardHoverClass() {
   board.classList.remove(X_CLASS);
   board.classList.remove(CIRCLE_CLASS);
   if (circleTurn) {
@@ -39,4 +82,12 @@ function setBoardHover() {
   } else {
     board.classList.add(X_CLASS);
   }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
 }
